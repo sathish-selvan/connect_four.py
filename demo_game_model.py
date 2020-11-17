51084b754974c9a5390fb8a913056877693ad6eb
thedoodler <ramprakashsmail@gmail.com>
2020-11-17 11:13:28 +0530

    winner bug fix

--- demo_game_model.py
+++ demo_game_model.py
@@ -11,46 +11,48 @@ def is_wins(empty_list):
             if empty_list[0+i][0] == 1 :
                 print("player one wins")
                 return False
-                
+
             elif empty_list[0+i][0] == 2 :
                 print("player two wins")
-                
+
                 return False
-                
-            
-    #vertical check:  
+
+
+    #vertical check:
     for j in range(0,4):
         if empty_list[0][0+j] == empty_list[1][0+j] == empty_list[2][0+j] == empty_list[3][0+j] and empty_list[0][0+j] != 0 :
             if empty_list[0][0+j] == 1 :
                 print("player one wins")
-                
+
                 return False
-                
+
             elif empty_list[0][0+j] == 2 :
                 print("player two wins")
                 return False
-                
+
     #diagonal check:
     if empty_list[0][0] == empty_list[1][1] == empty_list[2][2] == empty_list[3][3] and empty_list[0][0] != 0 :
         if empty_list[0][0] == 1 :
             print("player one wins")
             return False
-            
+
         elif empty_list[0][0] == 2 :
             print("player two wins")
             return False
-            
+
     if empty_list[0][3] == empty_list[1][2] == empty_list[2][1] == empty_list[3][0] and empty_list[0][3] != 0 :
         if empty_list[0][3] == 1 :
             print("player one wins")
             return False
-            
+
         elif empty_list[0][3] == 2 :
             print("player two wins")
             return False
+    else:
+        return True
 
-    
-            
+
+
 def check_win(board):
     empty_list = []
     for k in range(0,4):
@@ -59,9 +61,15 @@ def check_win(board):
                 empty_list.append(list(board[0+i+j][0+k:4+k]))
                 #print(empty_list)
 
-            is_wins(empty_list)
-            empty_list.clear()
-    
+            if is_wins(empty_list)==False:
+                return "end"
+            else:
+                empty_list.clear()
+
+            # print(empty_list)
+            # a = input()
+
+
 
 
 #a = int(input("Enter the number 0-6: "))
@@ -69,31 +77,41 @@ def check_win(board):
 #print(board)
 turn = 0
 flag = True
-while flag:
-    if turn % 2 == 0:
-        a = int(input("Enter the number 0-6: "))
-        if board[0][a] == 0:
-            for i in range(5,-1,-1):
-                if board[i][a] == 0:
-                    board[i][a] = 1
-                    check_win(board)
-                    turn += 1
-                    break
-            
-            print(board)
 
-        else:
-            print("Please enter valid input")
-    if turn %2 != 0:
-        b = int(input("Enter the number 0-6: "))
-        if board[0][b] == 0:
-            for i in range(5,-1,-1):
-                if board[i][b] == 0:
-                    board[i][b] = 2
-                    check_win(board)
-                    turn += 1
-                    break
-            
-            print(board)
-        else:
-            print("Please enter valid input")
+def main_loop(flag,turn,board):
+    while flag:
+        if turn % 2 == 0:
+            a = int(input("Enter the number 0-6: "))
+            if board[0][a] == 0:
+                for i in range(5,-1,-1):
+                    if board[i][a] == 0:
+                        board[i][a] = 1
+                        result = check_win(board)
+                        if result == "end":
+                            board = np.zeros((6,7))
+                            main_loop(True,0,board)
+                        turn += 1
+                        break
+
+                print(board)
+
+            else:
+                print("Please enter valid input")
+        if turn %2 != 0:
+            b = int(input("Enter the number 0-6: "))
+            if board[0][b] == 0:
+                for i in range(5,-1,-1):
+                    if board[i][b] == 0:
+                        board[i][b] = 2
+                        result = check_win(board)
+                        if result == "end":
+                            board = np.zeros((6,7))
+                            main_loop(True,0,board)
+                        turn += 1
+                        break
+
+                print(board)
+            else:
+                print("Please enter valid input")
+
+main_loop(flag,turn,board)
